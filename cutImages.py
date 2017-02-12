db5d6d60b62aad2e9da7f3170ad387c218d3df42
Aymen <alashwal.aymen@gmail.com>
2017-02-12 19:54:40 +0800

    add module for cutting images based on txt file provided with DataSet refactor foodDS file

---
+++ cutImages.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import os
+
+
+def read_bb_info(file_path):
+    file_path += 'bb_info.txt'
+    d = {}
+    with open(file_path) as f:
+        for line in f:
+            a = line.split()
+            if a[0] != 'img':
+                d[a[0]] = {'x1': int(a[1]), 'y1': int(a[2]), 'x2': int(a[3]), 'y2': int(a[4])}
+    return d
+
+
+def cut_image(image_path):
+    image_name = os.path.basename(image_path)
+    folder_path = image_path.replace(image_name, '')
+    im = Image.open(image_path)
+    img_dict = get_image_dict(folder_path, image_name.replace('.jpg', ''))
+    region = im.crop((img_dict['x1'], img_dict['y1'], img_dict['x2'], img_dict['y2']))
+    return region
+
+
+def get_image_dict(folder_path, image_name):
+    imgs_dict = read_bb_info(folder_path)
+    return imgs_dict[image_name]
+
+
+def show_cropped_img(image_path):
+    im = cut_image(image_path)
+    Image._show(im)
+
+# example.to.use
+# show_cropped_img('/media/aymen/DATA/datasets/UECFOOD100/1/35.jpg')
--- foodDS.py
+++ foodDS.py
@@ -8,44 +8,34 @@ from random import shuffle
 from os import listdir
 from os.path import isfile, join
 
-fullList = []
-for c in range(1, 6):
-    mypath = "/media/aymen/DATA/datasets/food-for-HPELM/" + str(c) + "/"
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print (len(onlyfiles), c)
-    for x in onlyfiles:
-        # print(mypath + x)
-        img = Image.open(mypath + x)
-        arr = array(img)
-        # print (arr.shape)
-        arr = imresize(arr, [64, 64, 3])
-        # print (arr.shape)
-        r = arr[:, :, 0].flatten()
-        g = arr[:, :, 1].flatten()
-        b = arr[:, :, 2].flatten()
-        label = [c]
-        # label = np.eye(np.max(label) + 1)[label]
-        # print (label)
-        out = np.array(list(label) + list(r) + list(g) + list(b), np.int)
-        # print (out.shape)
-        fullList.append(out)
-# print (len(fullList), fullList[0])
-shuffle(fullList)
-imagesList = np.array(fullList, dtype=int)
-print ("images shape: {}".format(imagesList.shape))
 
-# def unpickle(fileName):
-#     import cPickle
-#     fo = open(fileName, 'rb')
-#     dictList = cPickle.load(fo)
-#     fo.close()
-#     return dictList
-#
-#
+def read_images(path):
+    fullList = []
+    for c in range(1, 6):
+        mypath = path + str(c) + "/"
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        print (len(onlyfiles), c)
+        for x in onlyfiles:
+            img = Image.open(mypath + x)
+            arr = array(img)
+            arr = imresize(arr, [32, 32, 3])
+            r = arr[:, :, 0].flatten()
+            g = arr[:, :, 1].flatten()
+            b = arr[:, :, 2].flatten()
+            label = [c]
+            out = np.array(list(label) + list(r) + list(g) + list(b), np.int)
+            fullList.append(out)
+    shuffle(fullList)
+    imagesList = np.array(fullList, dtype=int)
+    print ("images shape: {}".format(imagesList.shape))
+    return imagesList
+
+
+imagesList = read_images("/media/aymen/DATA/datasets/food-for-HPELM/")
+
 batch_size = 200
-hidden_num = 2500
-#
-_inputs = 12288
+hidden_num = 2000
+_inputs = 3072
 _outputs = 6
 print("batch_size : {}".format(batch_size))
 print("hidden_num : {}".format(hidden_num))
@@ -56,50 +46,22 @@ elm.add_neurons(hidden_num, "tanh")
 while k <= 5:
     print("batch : {}".format(k))
     data = imagesList
-    print (data.shape, data.dtype, data)
+    print (data.shape, data.dtype)
     train_x = np.array(data[:(batch_size * k), 1:], dtype="int")
     train_y = np.array(data[:(batch_size * k), 0])
     print ("X", train_x.shape)
-    # print (train_x)
     print ("Y", train_y.shape)
-    # print (train_y)
     train_y = np.eye(np.max(train_y) + 1)[train_y]
-    print ("train_y", train_y)
     elm.train(train_x, train_y)
     k += 1
 
 end_time = time.time()
 
-testList = []
-for c in range(1, 6):
-    mypath = "/media/aymen/DATA/datasets/food-for-HPELM/test/" + str(c) + "/"
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print (len(onlyfiles), onlyfiles)
-    for x in onlyfiles:
-        # print(mypath + x)
-        img = Image.open(mypath + x)
-        arr = array(img)
-        # print (arr.shape)
-        arr = imresize(arr, [64, 64, 3])
-        # print (arr.shape)
-        r = arr[:, :, 0].flatten()
-        g = arr[:, :, 1].flatten()
-        b = arr[:, :, 2].flatten()
-        label = [c]
-        # label = np.eye(np.max(label) + 1)[label]
-        # print (label)
-        out = np.array(list(label) + list(r) + list(g) + list(b), np.int)
-        # print (out.shape)
-        testList.append(out)
-shuffle(testList)
-testImagesList = np.array(testList, dtype=int)
-# test = unpickle("./data/cifar-10-batches-py/test_batch")
+testImagesList = read_images("/media/aymen/DATA/datasets/food-for-HPELM/test/")
 print (testImagesList[:, 1:])
 test_x = np.array(testImagesList[:, 1:], dtype="int")
 print (testImagesList[:, 0])
 test_y = np.array(testImagesList[:, 0])
-# test_x = np.array(test['data'], dtype="int")
-# test_y = np.array(test['labels'])
 test_y = np.eye(np.max(test_y) + 1)[test_y]
 #
 Y = elm.predict(test_x)
